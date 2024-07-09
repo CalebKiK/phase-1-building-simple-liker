a1a0fb581808cd933592a740676a6a282c3d2d8d
CalebKiK <karimicaleb@gmail.com>
2024-07-09 19:39:02 +0300

    Complete lab

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const likeElement = document.querySelectorAll(".like-glyph");
+  const errorModal = document.getElementById("modal");
 
+  errorModal.classList.add("hidden");
 
+    likeElement.forEach((heart) =>
+      heart.addEventListener("click", () => {
+        mimicServerCall()
 
+        .then(() => {
+          if(heart.classList.contains("activated-heart")) {
+            heart.classList.remove("activated-heart");
+            heart.textContent = EMPTY_HEART;
+          } else{
+            heart.classList.add("activated-heart");
+            heart.textContent = FULL_HEART;
+          }
+        })
+
+        .catch((error) => {
+          errorModal.classList.remove("hidden");
+          errorModal.querySelector("#modal-message").textContent = error;
+        })
+      }
+      
+      ))}
+);
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
